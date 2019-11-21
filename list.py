29451feb49c8137cf9bd4b955cc721b5a9affc0b
asemqaffaf <asem@qaffaf.com>
2019-11-21 14:58:26 +0200

    looping

---
+++ list.py
@@ -0,0 +1,45 @@
+# 1
+list = [1,2,3,4,5,6,7,8,9]
+# for e in list:
+#     print(e)
+# 2
+# print(sum(list))
+# 3
+# print(max(list))
+# 4
+# a = [10,20,30,20,10,50,60,40,80,50,40]
+# a.sort()
+# a2 = []
+# for i in range(len(a)-1):
+#    if(a[i] != a[i+1]):
+#         a2.append(a[i])
+#
+# print(a2)
+# 5
+# if(len(list) ==0 ):
+#     print("it's empty")
+# 6
+# tuple = 1,2,3,"a",[1,2,4]
+# for e in tuple:
+#     print(e)
+# 7
+# num_set= set([0, 1, 2, 3, 4, 5])
+# for e in num_set:
+#     print(e)
+# 8
+# num_set1 = set([0,1,2,3,6,7])
+# num_se2 = set([9,10])
+# print(num_set1 | num_se2)
+# 9
+# it will print green blue yellow
+# 10
+# the length is 6
+# 11
+# the output will be 1:10 , 2:20 , 3:30 , 4:40,5:50,6:60
+# 12
+# e
+# llo,
+#
+# 13
+# hello, world!
+# Jello, World!
