a34d31d6be3818438bb4fe01ec90ae3af88cf828
asemqaffaf <asem@qaffaf.com>
2019-11-24 15:27:01 +0200

    map lambda filter

---
+++ lambda map filter.py
@@ -0,0 +1,40 @@
+# 1
+# the output will be 6 then 15 then 33
+# 2
+# from functools import reduce
+# multi = reduce(lambda a,b: a*b , [1,2,3,4,5,6,7,8])
+# print(multi)
+# 3
+# multiLambda =lambda a,b : a*b
+# print(multiLambda(10,22))
+# 4
+# postiveNumber = list(filter(lambda a: a>0 , range(-5,5)))
+# print(postiveNumber)
+# 5
+# output will be 13
+# 6
+# output will be [("Joey","Chandler","David"), ("Monica" , "Phoeble" , "Rachel")]
+# 7
+# output will be [("Bitcoin","BTC"), ("Ether" , "ETH" ), ("Ripple" , "XRP") , ("Litecoin" , "LTC")]
+# 8
+# output will be e o
+# 9
+# output will be List of students: 1 20 10
+# 10
+# output will be 1 4 9 16
+# 11
+# output will be [3 , 6 , 8]
+# 12
+# [6, 8]
+# output will be [6, 8]
+# 13
+# output will be [4, 8 , 12 , 16]
+# 14
+# from functools import reduce
+# x = reduce(lambda x,y :x if x<y else y,[1,2,1,1,2,2,2,2,0])
+# print(x)
+# 15
+# numbers = [1,2,3]
+# letters = ['a','b','c']
+# x = list(zip(numbers,letters))
+# print(x)
