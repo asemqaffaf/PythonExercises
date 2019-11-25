01136d74ca1c600cbd838814bc6b17cf54b71eec
asemqaffaf <asem@qaffaf.com>
2019-11-25 15:22:57 +0200

    oop

--- oop.py
+++ oop.py
@@ -1,32 +1,32 @@
 class Employee:
     empNumber : int
-    name : str
-    address : str
-    salary : float
-    jobTitle : str
+    __name : str
+    __address : str
+    __salary : float
+    __jobTitle : str
     def __init__(self,empNumber,name,address,salary,jobTitle):
         self.empNumber = empNumber
-        self.name = name
-        self.address = address
-        self.salary = salary
-        self.jobTitle = jobTitle
+        self.__name = name
+        self.__address = address
+        self.__salary = salary
+        self.__jobTitle = jobTitle
     def getName(self):
-        return self.name
+        return self.__name
     def getAdress(self):
-        return self.address
+        return self.__address
     def setAdress(self,address):
-        self.address = address
+        self.__address = address
     def getSalary(self):
-        return self.salary
+        return self.__salary
     def getJobTitle(self):
-        return self.jobTitle
+        return self.__jobTitle
     def __del__(self):
-        print("name of Employee " + self.name + "has been deleted")
+        print("name of Employee " + self.__name + "has been deleted")
     def __str__(self):
-        output = "Employee info \n .Emplyee number: "+ str(self.empNumber) + "\n .Name "+ self.name + "\n .Address "+ self.address + "\n .Salary" +str( self.salary) + "\n .Job title " + self.jobTitle
+        output = "Employee info \n .Emplyee number: "+ str(self.empNumber) + "\n .Name "+ self.__name + "\n .Address "+ self.__address + "\n .Salary" +str( self.__salary) + "\n .Job title " + self.__jobTitle
         return output
     def nonFormatedOutput(self):
-        output = "Employee info  .Emplyee number: "+ str(self.empNumber) + " .Name "+ self.name + " .Address "+ self.address + " .Salary" +str( self.salary) + ".Job title " + self.jobTitle
+        output = "Employee info  .Emplyee number: "+ str(self.empNumber) + " .Name "+ self.__name + " .Address "+ self.__address + " .Salary" +str( self.__salary) + ".Job title " + self.__jobTitle
 Employee1 = Employee(1,"mohammad Khalid","amman jordan","500","consultant")
 print(Employee1)
 Employee1.setAdress("USA")
